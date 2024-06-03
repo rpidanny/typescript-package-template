13be3fccc3b21a8076a2c81481cfdfb833ac478a
rpidanny <abhishekmaharjan1993@gmail.com>
2024-06-03 18:49:59 +0200

    feat: update tsconfig and jest configs

---
+++ jest.config.ts
@@ -0,0 +1,53 @@
+import { defaults } from 'jest-config'
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: defaults.testRegex,
+  transform: {
+    '^.+\\.(t|j)s$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: '<rootDir>/test/tsconfig.json',
+      },
+    ],
+  },
+  testEnvironment: 'jest-environment-node',
+  testPathIgnorePatterns: [
+    '<rootDir>/dist/',
+    '<rootDir>/tmp/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/local_tests/',
+  ],
+  testTimeout: 20_000,
+  setupFiles: ['<rootDir>/test/setup-test.ts'],
+  // collectCoverageFrom: ['**/*.(t|j)s'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.(t|j)s'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: [['lcov', { projectRoot: './' }], 'text'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/test/',
+    '<rootDir>/dist/',
+    '<rootDir>/tmp/',
+    '<rootDir>/local_tests/',
+    '<rootDir>/coverage/',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
+}
+
+export default config
